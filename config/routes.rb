54ba29a2dd99c2061ca70bad2373587eaab47558
rin0207 <rintaku.7@gmail.com>
2020-05-23 21:58:21 +0900

    [start]3-6

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
-	get 'top' => 'homes#top'
+  get 'todolists/new'
+
+  get 'top' => 'homes#top'
+
+  post 'todolists' => 'todolists#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
